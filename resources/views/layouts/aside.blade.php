f6fcdf9445186e03cc99a316f67107e8e5433679
Kaung Si Thu <kaungsithugalaxynote10plus@gmail.com>
2026-01-15 23:16:02 +0700

    latest

--- resources/views/layouts/aside.blade.php
+++ resources/views/layouts/aside.blade.php
@@ -1,11 +1,11 @@
 <aside id="sidebar" class="sidebar">
     <ul class="sidebar-nav" id="sidebar-nav">
-        <li class="nav-item">
+        {{-- <li class="nav-item">
             <a class="nav-link" href="">
                 <i class="bi bi-grid"></i>
                 <span>Dashboard</span>
             </a>
-        </li>
+        </li> --}}
         <li class="nav-item">
             <a class="nav-link collapsed" href="#employee-nav" data-bs-toggle="collapse" aria-expanded="false">
                 <i class="bi bi-menu-button-wide"></i>
@@ -15,9 +15,9 @@
             <ul id="employee-nav" class="nav-content collapse">
                 <li><a href="{{ route('employees.index') }}"><i class="bi bi-circle"></i><span>Employees</span></a></li>
                 {{-- <li><a href="{{ route('emp_exp.index') }}"><i class="bi bi-circle"></i><span>Employees Work Experiences</span></a></li> --}}
-                <li><a href="{{ route('attendances.index') }}"><i class="bi bi-circle"></i><span>Attendance</span></a>
+                {{-- <li><a href="{{ route('attendances.index') }}"><i class="bi bi-circle"></i><span>Attendance</span></a> --}}
                 </li>
-                <li><a href="{{ route('leaves.index') }}"><i class="bi bi-circle"></i><span>Leaves</span></a></li>
+                <li><a href="{{ route('leaves.index') }}"><i class="bi bi-circle"></i><span>Leaves / Attendance </span></a></li>
                 {{-- <li><a href="{{ route('leaves.calendar') }}"><i class="bi bi-circle"></i><span>Leaves</span></a></li> --}}
                 <li><a href="{{ route('leave-types.index') }}"><i class="bi bi-circle"></i><span>Leaves Type</span></a></li>
                 <li><a href="{{ route('recruitments.index') }}"><i
