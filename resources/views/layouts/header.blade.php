cedad91743343379234a1224703db61389e3d34f
Kaung Si Thu <kaungsithugalaxynote10plus@gmail.com>
2026-01-16 10:40:09 +0700

    logo fixed

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,12 +1,16 @@
-<header id="header" class="header fixed-top d-flex align-items-center" style="background-color:#f1f2f4">
-  <div class="d-flex align-items-center">
-    <div class="d-flex align-items-center py-5 px-3"> 
-    <a href="" class="logo d-flex align-items-center">
-      <img src="{{asset('assets/img/mon-state-logo.png')}}" alt="" style="max-height: 60px;">
-      <span class="d-none d-lg-block ms-3">{{config('app.name')}}</span>
+<header id="header" class="header fixed-top d-flex align-items-center" style="background-color:#f1f2f4; height: 80px;">
+  <div class="d-flex align-items-center px-3"> <a href="" class="logo d-flex align-items-center">
+      <img src="{{asset('assets/img/mon-state-logo.png')}}" alt="Logo" style="height: 60px; width: auto;">
+      <span class="d-none d-lg-block ms-3" style="font-size: 26px; font-weight: 700; color: #012970;">
+        {{config('app.name')}}
+      </span>
     </a>
-    <i class="bi bi-list toggle-sidebar-btn ms-auto"></i> </div>
-  </div><!-- End Logo -->
+
+    <i class="bi bi-list toggle-sidebar-btn ms-4" style="font-size: 30px; cursor: pointer;"></i>
+    
+  </div>
+
+
 
   <div class="search-bar">
     {{-- <form class="search-form d-flex align-items-center" method="POST" action="#">
